f70b2fc00b71f97ecd0bba9b15a3e655fccc67bf
spopalzay <s.popalzay@outlook.com>
2021-04-22 00:01:51 +0100

    Navbar and home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  quotes = ['“No amount of physical contact could match the healing powers of a well made cocktail.” — David Sedaris.',
+  '“Hear no evil, speak no evil, and you won’t be invited to cocktail parties.” — Oscar Wilde',
+  '“There are two kinds of people I don’t trust: people who don’t drink and people who collect stickers.” — Chelsea Handler',
+  '“Alcohol may be man’s worst enemy, but the bible says love your enemy.” — Frank Sinatra',
+  '"A cocktail done right can really show your guests that you care." — Danny Meyer',
+  '“Life is a crazy mixture of intoxicating cocktails.” ― Ken Poirot',
+  '“Cocktails don’t solve any problems, but then again neither does water.” ― Emily Beckett'];
+  randomQuote = '';
   constructor() { }
-
+  
   ngOnInit(): void {
+    let quoteIndex = Math.floor(Math.random() * (this.quotes.length));
+     this.randomQuote = this.quotes[quoteIndex];
   }
+
+
 
 }
